28cab9ce81aac680639ec3e2a8ba566c6ae40cc0
Tito otniel <titootniel26@gmail.com>
2021-10-08 09:08:32 +0700

    Commit-2

---
+++ resources/views/form.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+<body>
+    <div class="container-fluid">
+
+        <table class="table mt-3 mb-4">
+            <thead>
+                <tr>
+
+                    <th scope="col">FORM PENDAFTARAN</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope=" row">Nama : <input type="text" placeholder="Nama"></th>
+
+                </tr>
+                <tr>
+                    <th scope="row">Tanggal Lahir : <input type="date" placeholder="Alamat"></th>
+                    <td></td>
+
+                </tr>
+                <tr>
+                    <th scope="row">Alamat : <input type="textarea" placeholder="Alamat"> </th>
+                </tr>
+                <tr>
+                    <th scope="row">Jenis Kelamin : <input type="checkbox" placeholder="Alamat"> Laki-Laki <input type="checkbox" placeholder="Alamat"> Perempuan </th>
+                </tr>
+
+                <tr>
+                    <th scope="row"> <input type="submit" value="Submit"> <input type="submit" value="Cancel"></th>
+                </tr>
+        </table>
+    </div>
+</body>
+
+</html>
